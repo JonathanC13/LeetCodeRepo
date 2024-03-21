f8fa95264dc2cdca94a8e28e62c087e329a08117
jonathanchan <16930415+JonathanC13@users.noreply.github.com>
2024-03-20 21:36:03 -0400

    Am I 5 years old?

---
+++ Grind75_1/week1/TwoSum.py
@@ -0,0 +1,55 @@
+"""
+https://leetcode.com/problems/two-sum/
+"""
+
+from typing import List
+
+class Solution:
+
+    # O(n)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        dictNums = {}
+        iDiff = None
+
+        for x in range(0, len(nums)):
+            iDiff = target - nums[x]
+            if (dictNums.get(iDiff) is not None):
+                return [x, dictNums.get(iDiff)]
+            else:
+                # add the arr value as the 'key' and the index as the 'value'.
+                # Since 2 sum, if multiple instances of same number it is OK to overwrite the index in the 'value'.
+                dictNums[nums[x]] = x
+
+        return []
+    
+    # Brute force, O(n^2)
+    def twoSumBrute(self, nums: List[int], target: int) -> List[int]:
+
+        res = []
+
+        iDiff = None
+        iCurrIdx = 0
+
+        iLenNums = len(nums)
+
+        for x in range(0, iLenNums):
+            iDiff = target - nums[x]
+            if (iDiff == nums[x] and nums.count(iDiff) == 1):
+                # must be the same index
+                pass
+            elif (nums.count(iDiff) > 0):
+                # find the index of the 2nd number that will sum to the target
+                for y in range(0, iLenNums):
+                    if (x == y):
+                        pass
+                    elif (nums[y] == iDiff):
+                        res.append(x)
+                        res.append(y)
+                        return res
+            else:
+                pass    
+        return res
+
+  
+    
