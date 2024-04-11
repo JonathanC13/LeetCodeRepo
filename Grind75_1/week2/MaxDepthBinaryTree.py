21d68ac19611163048045f1155b60637a0a7372f
jonathanchan <16930415+JonathanC13@users.noreply.github.com>
2024-04-11 19:44:06 -0400

    wow wee

---
+++ Grind75_1/week2/MaxDepthBinaryTree.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+        if (root is None):
+            return 0
+
+        # search left children
+        leftDepth = self.maxDepth(root.left)
+
+        # search right children
+        rightDepth = self.maxDepth(root.right)
+
+        # from the left and right branch, return the longer one + 1 for this node
+        return max(leftDepth, rightDepth) + 1
