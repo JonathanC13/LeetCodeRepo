7a6edf23d644bea87347444ff9ffe5dd3293d03b
jonathanchan <16930415+JonathanC13@users.noreply.github.com>
2024-05-04 13:24:41 -0400

    hooray

--- Grind75_1/week4/RottingOranges.py
+++ Grind75_1/week4/RottingOranges.py
@@ -62,6 +62,60 @@ class Solution:
 
         return mins
 
+
+    def orangesRottingDFS(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        if(rows == 0):
+            return -1;
+
+        # 0 for empty, 1 for fresh, so start at 2. At end -2 to get the actual minutes
+        minutes = 2
+
+        def helper(grid: List[List[int]], r: int, c: int, mins: int):
+            
+            if (r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0 or (grid[r][c] >= 2 and grid[r][c] < mins)):
+                # out of bounds
+                # empty cell
+                # orange has already rotted from another orange. i.e. the target cell has a minute value lesser than the current minute being evaluated meaning this orange can be rotted earlier.
+                return
+
+            # place the minimum value for this specific orange to rot
+            grid[r][c] = mins
+
+            # check left, top, right, bot
+            helper(grid, r, c - 1, mins + 1)
+            helper(grid, r - 1, c, mins + 1)
+            helper(grid, r, c + 1, mins + 1)
+            helper(grid, r + 1, c, mins + 1)
+
+            return
+
+        # for each rotton orange, spread the rot
+        for r in range(rows):
+            for c in range(cols):
+                if (grid[r][c] == 2):
+                    # when a rotted orange is found, spread the rot and the value in the rotted orange is the minute that orange becomes rotted.
+                    helper(grid, r, c, 2)
+
+        for r in range(rows):
+            print(grid[r])
+
+        for r in range(rows):
+            for c in range(cols):
+                # the highest value in the grid is the minute that last possible orange was rotted.
+                minutes = max(grid[r][c], minutes)
+                if (grid[r][c] == 1):
+                    # if there is still a fresh orange, ret -1
+                    return -1
+
+        
+
+        return minutes - 2
+
+
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        return self.orangesRottingBFS(grid)
-        # todo recursive soln
+        #return self.orangesRottingBFS(grid)
+        return self.orangesRottingDFS(grid)
+        
