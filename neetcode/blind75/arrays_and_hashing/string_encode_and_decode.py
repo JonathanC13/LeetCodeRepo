31ce8b57b4a06a05e8846de1bfbcc011539073ed
jonhs <16930415+JonathanC13@users.noreply.github.com>
2024-09-12 12:30:46 -0400

    a

---
+++ neetcode/blind75/arrays_and_hashing/string_encode_and_decode.py
@@ -0,0 +1,29 @@
+# https://neetcode.io/problems/string-encode-and-decode
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + '#' + s
+
+        return res
+ 
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+
+            s_len = s[i:j]
+            i = j + 1
+            j = i + int(s_len)
+            res.append(s[i:j])
+
+            i = j
+
+        return res
+
+
